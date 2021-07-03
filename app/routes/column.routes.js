34d22694960de1d8a0a9f5de21115e996155fd94
BP <bhavikpareek98@gmail.com>
2021-07-03 16:25:07 +0530

    express and sequelize app

---
+++ app/routes/column.routes.js
@@ -0,0 +1,17 @@
+module.exports = app => {
+    const columns = require("../controllers/column.controller.js");
+
+    var router = require("express").Router();
+  
+    router.post("/", columns.create);
+  
+    router.get("/", columns.findAll);
+
+    router.get("/:id", columns.findOne);
+ 
+    router.put("/:id", columns.update);
+  
+    router.delete("/:id", columns.delete);
+
+    app.use('/api/columns', router);
+  };
